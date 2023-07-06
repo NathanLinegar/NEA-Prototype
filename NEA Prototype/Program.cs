aadaeb4f3b0c386a902658d6d3cd5b3cfe09359d
NathanLinegar <22NL1827@barton.ac.uk>
2023-07-06 13:14:36 +0100

    Installed Packages
    I've installed the necessary packages for the program to work.

---
+++ NEA Prototype/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace NEA_Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
