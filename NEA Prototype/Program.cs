db70ad38f09ec07b952776d9a7b7d49426ce83ba
NathanLinegar <22NL1827@barton.ac.uk>
2023-07-12 10:17:27 +0100

    Made some prototype code
    Title says it all

--- NEA Prototype/Program.cs
+++ NEA Prototype/Program.cs
@@ -11,6 +11,35 @@ namespace NEA_Prototype
     {
         static void Main(string[] args)
         {
+            int UserInput;
+            MainMenu();
+            UserInput = int.Parse(Console.ReadLine());
+
+            ViewDataBase();
+            Console.ReadKey();
+        }
+        static void LoginMenu()
+        {
+
+        }
+        static void MainMenu()
+        {
+            Console.WriteLine("1) View Entries in the database");
+            Console.WriteLine("2) Add an entry to the database");
+            Console.WriteLine("3) Remove an entry from the database");
+        }
+        static void ViewDataBaseChoices()
+        {
+            Console.WriteLine("Which entries do you wish to be displayed");
+            Console.WriteLine("1) All employees");
+            Console.WriteLine("2) Employees with a certain name (fore/sur)");
+            Console.WriteLine("3) Employees with IDs within a specified range");
+            Console.WriteLine("4) Not implemented yet");
+        }
+        static void ViewDataBase()
+        {
+            ViewDataBaseChoices();
+            SQLiteConnection conn = new SQLiteConnection("Driver = {RotaSystemDataBase.db}");
         }
     }
-}
+} 
