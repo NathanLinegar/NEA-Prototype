62da01976f8ffc7ba8d2f6f45645bb3c300c20a1
NathanLinegar <22NL1827@barton.ac.uk>
2023-10-02 09:46:48 +0100

    Made the employee class and started work on getting the login and reading the employee data from the database

---
+++ NEA Prototype/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEA_Prototype
+{
+    internal class Employee
+    {
+        protected int EmployeeID;
+        public string forename;
+        public string surname;
+        protected string emailAdd;
+        protected string password;
+        protected string phoneNum;
+        protected DateTime DOB;
+        protected bool acceptExtraShifts;
+        protected float Wage;
+
+
+        public Employee() { }
+    }
+}
--- NEA Prototype/Program.cs
+++ NEA Prototype/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace NEA_Prototype
 {
@@ -20,7 +22,7 @@ namespace NEA_Prototype
         }
         static void LoginMenu()
         {
-
+            //lmao i'll get this done sometime
         }
         static void MainMenu()
         {
@@ -34,12 +36,73 @@ namespace NEA_Prototype
             Console.WriteLine("1) All employees");
             Console.WriteLine("2) Employees with a certain name (fore/sur)");
             Console.WriteLine("3) Employees with IDs within a specified range");
-            Console.WriteLine("4) Not implemented yet");
         }
         static void ViewDataBase()
         {
+            int Selection, rangeStartVal, rangeEndVal;
+            string query = "", userinput, name = "";
             ViewDataBaseChoices();
-            SQLiteConnection conn = new SQLiteConnection("Driver = {RotaSystemDataBase.db}");
+            try
+            {
+                Selection = int.Parse(Console.ReadLine());
+
+                if (Selection == 1)
+                {
+                    query = "SELECT * FROM Employees";
+                }
+                else if (Selection == 2)
+                {
+                    Console.WriteLine("Input a forename or surname");
+                    userinput = Console.ReadLine();
+                    for (int i = 0; i < userinput.Length; i++)
+                    {
+                        if (i == 0)
+                        {
+                            name += userinput[i].ToString().ToUpper();
+                        }
+                        else
+                        {
+                            name += userinput[i].ToString().ToLower();
+                        }
+                    }
+                    query = $"SELECT * FROM Employees WHERE forename = '{name}' OR surname = '{name}'";
+                }
+                else if (Selection == 3)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+
+
+
+
+                using (SQLiteConnection conn = new SQLiteConnection("Data Source = RotaSystemDataBase.db"))
+                {
+                    conn.Open();
+                    float num;
+                    num = (float)1067878/333;
+                    SQLiteCommand cmd = new SQLiteCommand(query, conn);
+                    using (SQLiteDataReader rdr = cmd.ExecuteReader())
+                    {
+                        while (rdr.Read())
+                        {
+                            Console.WriteLine("1");
+                            Console.ReadKey();
+         
+                            Console.WriteLine(num);
+                            Console.ReadKey();
+                        }
+                    }
+                }
+            }
+            catch 
+            { 
+            
+            
+            }
         }
     }
 } 
